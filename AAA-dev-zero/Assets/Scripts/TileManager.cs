8c9f271771b66b3fffbeb755248db5e87660b953
Sw4gn3m1te <woingenau.lvx@web.de>
2019-11-30 00:45:53 +0100

    added script for spawning

---
+++ AAA-dev-zero/Assets/Scripts/TileManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] tileprefabs;
+    public int i = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (i <= 9)
+        {
+            GameObject x;
+            x = Instantiate(tileprefabs[i]) as GameObject;
+            i++;
+        }
+    }   
+}
