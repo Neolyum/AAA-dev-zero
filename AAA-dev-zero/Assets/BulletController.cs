bc5c289ef9ef39c1bd25d1e649cdfd28a99351f4
Varsius <42544122+Varsius@users.noreply.github.com>
2019-11-30 18:13:07 +0100

    Addes shooting

---
+++ AAA-dev-zero/Assets/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float recoil = 25f;
+
+    private void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = transform.right * shootingSpeed;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(GetComponent<Rigidbody2D>().velocity * recoil, ForceMode2D.Impulse);
+        }
+        Destroy(gameObject);
+    }
+}
