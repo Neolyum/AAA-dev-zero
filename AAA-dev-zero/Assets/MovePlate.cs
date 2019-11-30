eaba8c6d6426ec7d64d79b53bd6c25c6f3422eaf
Neolyum <jojojonasjojo@gmail.com>
2019-11-30 13:37:45 +0100

    added moving plates

---
+++ AAA-dev-zero/Assets/MovePlate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+    [SerializeField] private int range = 3;
+    private int sign = 1; //1 or -1
+    private float moved = 0;
+    [SerializeField] private float speed = 1.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.deltaTime;
+
+        transform.Translate(speed * time * sign, 0, 0);
+        moved += time;
+
+        if (moved > range)
+        {
+            moved = 0;
+            sign *= -1;
+        }
+    }
+}
