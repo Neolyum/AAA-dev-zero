25c912071b70c59ac4b87b759e316d51ecd6f007
Neolyum <jojojonasjojo@gmail.com>
2019-11-30 20:45:27 +0100

    began player recognizing

--- AAA-dev-zero/Assets/Scripts/GuiController.cs
+++ AAA-dev-zero/Assets/Scripts/GuiController.cs
@@ -22,11 +22,29 @@ namespace Controller
                 GUI.Label(new Rect(Screen.width / 4f, Screen.height / 8f, 500, 500), "Game Over!\nWinner is: " + winner, myStyle);
             }
             else GUI.Label(new Rect(Screen.width / 4f, Screen.height / 8f, 500, 500), "\t\t\t\t\n\n\n ", myStyle);
+
+            if (players.Length > 0)
+            {
+                GUI.Label(new Rect(10, 10, 50, 50), "Player1");
+            }
+            if (players.Length > 1)
+            {
+                GUI.Label(new Rect(Screen.width -10 -50, 10, 50, 50), "Player2");
+            }
+            if (players.Length > 2)
+            {
+                GUI.Label(new Rect(10, Screen.height -10-50, 50, 50), "Player3");
+            }
+            if (players.Length > 3)
+            {
+                GUI.Label(new Rect(Screen.width - 10 - 50, Screen.height - 10 - 50, 50, 50), "Player4");
+            }
         }
 
         private void Update()
         {
-            players = GameObject.FindGameObjectsWithTag("Players");
+            players = GameObject.FindGameObjectsWithTag("Player");
+
         }
         private void Start()
         {
