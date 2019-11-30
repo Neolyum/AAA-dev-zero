cc412171c39727e647fb693e052eaad7c2ffb2e3
Neolyum <jojojonasjojo@gmail.com>
2019-11-30 11:35:51 +0100

    camera is moving now

--- AAA-dev-zero/Assets/Scripts/CameraController.cs
+++ AAA-dev-zero/Assets/Scripts/CameraController.cs
@@ -5,26 +5,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-
-    public float speed = 2;
-    public GameObject[] tileprefabs;
+    private float speed = 2;
     public Camera MainCamera;
-   
-    void Start()
-    {
-        Instantiate(tileprefabs[0], new Vector3(2,2,2),Quaternion.identity);
-    }
+
 
     // Update is called once per frame
-    void Update()
-
-
-        
-    {
-        
-        Vector2 offset = new Vector2(Time.time * speed, 1);
-        this.transform.position = offset;
+    void Update() {
+        transform.Translate(new Vector2(Time.deltaTime * speed, 0));
     }
 }
